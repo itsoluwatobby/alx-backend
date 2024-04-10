11bc7f5eeca6b64f0ba69d4a26c6c3f91d3e5df7
itsoluwatobby <oluwasamuel050@gmail.com>
2024-04-10 03:14:52 +0100

    chore(task-started): internationalization task continuation

--- 0x02-i18n/routes/route5.py
+++ 0x02-i18n/routes/route5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic route task 1"""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 
 
 app_routes = Blueprint('app_routes', __name__)
@@ -9,4 +9,4 @@ app_routes = Blueprint('app_routes', __name__)
 @app_routes.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """A basic endpoint that returns an home page"""
-    return render_template('1-index.html')
+    return render_template('5-index.html', user=g.user)
