62647e1eed36631195fb59e338d8a8872717120e
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-04-10 04:11:29 +0100

    Add files via upload

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""module 2-app.py"""
+from flask import Flask, request
+from flask_babel import Babel
+from routes.route4 import app_routes
+from config import Config
+from typing import Union
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+app.config.from_object(Config)
+app.register_blueprint(app_routes)
+
+
+@babel.localeselector
+def get_locale() -> Union[str, None]:
+    """Determine best match for a supported languages
+    """
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    """Runs the basic flask app"""
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
